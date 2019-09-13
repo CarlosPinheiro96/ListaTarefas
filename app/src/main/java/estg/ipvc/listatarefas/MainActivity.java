ba45f91139457b267f6341185f312cdf5f3da084
CMCP1996 <CMCP1996@users.noreply.github.com>
2019-09-13 14:44:58 +0100

    Change background color and new View

--- app/src/main/java/estg/ipvc/listatarefas/MainActivity.java
+++ app/src/main/java/estg/ipvc/listatarefas/MainActivity.java
@@ -2,14 +2,17 @@ package estg.ipvc.listatarefas;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -118,8 +121,25 @@ public class MainActivity extends AppCompatActivity {
 
             itens = new ArrayList<String>();
             ids = new ArrayList<Integer>();
-            tarefasAdaptor = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_2, android.R.id.text2,itens);
+            listaTarefa.setAdapter(tarefasAdaptor);
 
+
+
+            tarefasAdaptor = new ArrayAdapter<String>(getApplicationContext(),
+                    android.R.layout.simple_list_item_2,
+                    android.R.id.text2,
+                    itens){
+
+            @Override
+                public  View getView(int position, View convertView, ViewGroup parent) {
+
+
+                View view = super.getView( position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text2);
+                text.setTextColor(Color.BLACK);
+                return view;
+            }
+            };
 
             listaTarefa.setAdapter(tarefasAdaptor);
 
